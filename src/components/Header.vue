508bba64bd9e3e2de657bf3bd57ebd3b4b666ba0
Marcus Vinicius Silva Carceles <re037335@resource.com.br>
2019-01-04 15:01:38 -0200

    arquivos adicionados

---
+++ src/components/Header.vue
@@ -0,0 +1,33 @@
+<template>
+
+    <v-content>
+        <v-toolbar dark>
+            <v-btn flat color="cyan" value="Home">
+                <font-awesome-icon icon="igloo"/>
+            </v-btn>
+            <v-btn flat color="red">
+                <font-awesome-icon icon="eye"/>
+            </v-btn>
+            <v-spacer></v-spacer>
+            <v-btn flat color="green" value="Lançamento">
+                <font-awesome-icon icon="calendar-week"/>
+            </v-btn>
+            <v-btn flat color="pink" value="popular">
+                <font-awesome-icon icon="heart"/>
+            </v-btn>
+            <v-btn flat color="yellow" value="best">
+                <font-awesome-icon icon="glass-cheers"/>
+            </v-btn>
+        </v-toolbar>
+    </v-content>
+</template>
+
+<script>
+    export default {
+        name: "Header"
+    }
+</script>
+
+<style>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,25 @@
 import Vue from 'vue'
+import './plugins/vuetify'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 import './registerServiceWorker'
 
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core';
+ 
+// Either import only the icons you need like this:
+// import { faCoffee, faAngry, faGraduationCap } from '@fortawesome/free-solid-svg-icons';
+// library.add(faCoffee, faAngry, faGraduationCap);
+ 
+// Or import the whole sets you need and add it to the library!
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+
+library.add(fas, far, fab);
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 Vue.config.productionTip = false
 
 new Vue({
